723eaaba650750d69a4836943b77b8cb110876c6
Jon <wintersfile@gmail.com>
2019-05-20 16:53:04 -0700

    latest update in google o-auth

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ Gang: 'Shit Bitch' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
+//http://localhost:5000/auth/google/callback
+//mongodb+srv://admin:admin@guidents-ci9ky.mongodb.net/test?retryWrites=true
---
+++ services/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+		User.findOne({ googleID: profile.id })
+		 .then((existingUser) => {
+			 if(existingUser){
+				 // we already have a record with given profile ID
+			 } else{
+				 // we don't have a user record, make new one
+				 new User({ googleID: profile.id }).save();
+			 }
+		 });
+	})
+);
