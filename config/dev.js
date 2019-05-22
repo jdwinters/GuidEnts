5770e435d536ec0b5b9b1a7c2f8a408ff5cc591f
Jon <wintersfile@gmail.com>
2019-05-22 11:16:20 -0700

    Fuck Rasika

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js - don't commit to heroku
+module.exports = {
+	googleClientID: '311484430436-22ugfu41sh53ou88vgrhlqaqrd75vgv6.apps.googleusercontent.com',
+	googleClientSecret: 'LZsY7WzoD1NKtIcqSNHuSNgD',
+	mongoURI: 'mongodb+srv://admin:admin@guidents-ci9ky.mongodb.net/test?retryWrites=true',
+	cookieKey: 'dfdfjdfjbjdskjsjbfjbsehjrt9834'
+};
