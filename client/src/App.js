177993cd33bdf52cfcac080af4431de3aac11b67
Samuel Barish <sbarish@ucsc.edu>
2019-05-22 20:25:21 -0700

    added front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<h2>Hi there </h2>
+		<h1>Welcome! </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-		<a href="/auth/google">Sign In With Google</a>
+        <a href="/auth/google" class="button">Sign in With Google</a>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
