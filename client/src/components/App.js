b5a5e057a924625c80e5bbd92fb73f2e3aa5eb0b
Jon <wintersfile@gmail.com>
2019-05-24 11:28:35 -0700

    newest functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Header from './Header';
+import Landing from './Landing';
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
 	componentDidMount(){
@@ -19,7 +20,7 @@ class App extends Component {
 					<div>
 						<Header />
 						<Route exact path="/" component={Landing} />
-						<Route exact path="/surveys" component={Dashboard} />
+						<Route exact path="/dashboard" component={Dashboard} />
 						<Route path="/surveys/new" component={SurveyNew} />
 
 					</div>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const Landing = () => {
+	return(
+		<div style={{ textAlign: 'center' }}>
+			<h1>
+				GuidEnts
+			</h1>
+			Go On Your Academic Path
+		</div>
+	);
+};
+
+export default Landing;
