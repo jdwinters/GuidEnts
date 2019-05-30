9e2658466a246a7b9eb9ac87ac45ee013e39e882
Rasika Kale <rkale@ucsc.edu>
2019-05-30 09:45:34 -0700

    Added advisers.js class

--- client/src/components/Advisers.js
+++ client/src/components/Advisers.js
@@ -1,15 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import M from "materialize-css";
 
-const Advisers = () => {
-	return(
-		<div style={{ textAlign: 'center' }}>
-			<h1>
-				Advisers
-			</h1>
-			Go On Your Academic Path
-		</div>
-	);
-};
+class Advisers extends Component {
+  componentDidMount() {
+    M.AutoInit();
+  }
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Advisers</h1>
+        Go On Your Academic Path
+        <ul class="collapsible">
+          <li>
+            <div class="collapsible-header">
+              <p class="material-icons">Advisor 1</p>
+            </div>
+            <div class="collapsible-body">
+              <span>Phone</span> <br />
+              <span>Email</span>
+            </div>
+          </li>
+          <li>
+            <div class="collapsible-header">
+              <p class="material-icons">Advisor 2</p>
+            </div>
+            <div class="collapsible-body">
+              <span>Phone</span> <br />
+              <span>Email</span>
+            </div>
+          </li>
+          <li>
+            <div class="collapsible-header">
+              <p class="material-icons">Advisor 3</p>
+            </div>
+            <div class="collapsible-body">
+              <span>Phone</span> <br />
+              <span>Email</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// look at the materialize card page to make list of advisor contacts
 
 export default Advisers;
